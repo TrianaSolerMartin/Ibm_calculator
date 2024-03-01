a69e7f9dac1d07c1ef090d73d3a6db2786b71649
TrianaSolerMartin <triana287@gmail.com>
2024-03-01 12:34:41 +0100

    working

--- build/IbmCalculator.js
+++ build/IbmCalculator.js
@@ -4,11 +4,20 @@
 //peso
 //altura
 // ibm la cual es una funcion que reutiliza los datos de altura y peso 
+//utilizamos al comienz un void como resultado de nustra funcion 
 function IbmCalculator(altura, peso) {
     const ibm = peso / (altura ** 2);
-    return console.log(ibm);
+    if (ibm < 25) {
+        return ("estas super fit");
+    }
+    else if (ibm >= 25 && ibm >= 27) {
+        return ("ESTAS EN BUEN PESO");
+    }
+    else {
+        return ("tienes sobrepeso");
+    }
 }
-IbmCalculator(170, 74);
-IbmCalculator(179, 74);
-IbmCalculator(140, 74);
-IbmCalculator(190, 74);
+console.log(IbmCalculator(1.70, 64));
+console.log(IbmCalculator(1.90, 84));
+console.log(IbmCalculator(1.90, 94));
+console.log(IbmCalculator(1.70, 74));
