5d8f84ccb9719ca8bea9fb0e70123713e4753065
TrianaSolerMartin <triana287@gmail.com>
2024-03-01 11:55:14 +0100

    adding funtion ibm

--- IbmCalculator.ts
+++ IbmCalculator.ts
@@ -1,1 +1,19 @@
-console.log("hola typescript")
+// console.log("hola typescript")
+
+// hemos declarado 3 constantes
+
+//peso
+
+//altura
+
+// ibm la cual es una funcion que reutiliza los datos de altura y peso 
+
+function IbmCalculator ( altura : number, peso : number ) {
+    const ibm = peso / (altura**2);
+    return console.log(ibm);
+}
+
+IbmCalculator(170, 74)
+IbmCalculator(179, 74)
+IbmCalculator(140, 74)
+IbmCalculator(190, 74)
--- build/IbmCalculator.js
+++ build/IbmCalculator.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("hola typescript");
+// console.log("hola typescript")
+// hemos declarado 3 constantes
+//peso
+//altura
+// ibm la cual es una funcion que reutiliza los datos de altura y peso 
+function IbmCalculator(altura, peso) {
+    const ibm = peso / (altura ** 2);
+    return console.log(ibm);
+}
+IbmCalculator(170, 74);
+IbmCalculator(179, 74);
+IbmCalculator(140, 74);
+IbmCalculator(190, 74);
